c9a9643a719f7e194c89216a36bff10db033c2e2
lalitmee <lalitkumar.meena.lk@gmail.com>
2018-03-12 18:55:22 +0530

    changes

--- Codechef/Python/daysInMonth.py
+++ Codechef/Python/daysInMonth.py
@@ -3,18 +3,17 @@
 import sys
 
 for isdfrgti in range(int(input())):
-    s=list(map(str,input().strip().split(' ')))
-    ddd=['mon', 'tues', 'wed', 'thurs', 'fri', 'sat' , 'sun']
+    s = list(map(str, input().strip().split(' ')))
+    ddd = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun']
     for i in range(7):
-        if(ddd[i]==s[1]):
+        if(ddd[i] == s[1]):
             break
-    f=[0 for i in range(7)]
-    j=0
-    while(j<int(s[0])):
-        f[i]+=1
-        i=(i+1)%7
-        j+=1
+    f = [0 for i in range(7)]
+    j = 0
+    while(j < int(s[0])):
+        f[i] += 1
+        i = (i+1) % 7
+        j += 1
     for i in f:
-        print(i,end=' ')
+        print(i, end=' ')
     print()
-
