a1c0d1a0d4de615f2316d2dc775083e3d7ec26b7
lalitmee <lalitkumar.meena.lk@gmail.com>
2018-04-08 00:46:37 +0530

    removed extra spcaes from the file

--- HackerRank/Java/minMax.java
+++ HackerRank/Java/minMax.java
@@ -8,19 +8,19 @@ public class minMax {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
+
         Scanner in = new Scanner(System.in);
         long[] a = new long[5];
         long sum = 0;
-        
+
         for (int i = 0 ; i < a.length ; i++){
             long temp = in.nextLong();
             a[i] = temp;
             sum += temp;
         }
-    
+
         Arrays.sort(a);
         System.out.println((sum - a[a.length-1]) + " " + (sum - a[0]));
-        
+
     }
 }
